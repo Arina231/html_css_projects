d34d24dac147ea26cf0c3671c6fa561ad56753cf
Arina231 <arinahonaa119@gmail.com>
2023-03-14 23:31:53 +0000

    modified:   Project_1/js/script.js

--- Project_1/js/script.js
+++ Project_1/js/script.js
@@ -46,7 +46,7 @@ const obs = new IntersectionObserver(
 obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
-
+// Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
